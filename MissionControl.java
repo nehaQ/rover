76ce33a80c9629ae76832dc32476b8bd3625f10f
Neha Agrawal <neha.agrawal3093@gmail.com>
2014-03-20 15:44:03 +0000

    Read first line of input
    And initiliase 2d array

--- MissionControl.java
+++ MissionControl.java
@@ -1,21 +1,33 @@
 import java.util.Scanner; // Scanner makes it easier to parse strings
-// Create a new scanner object for each line because they are cheap to make;
-// thus easier to handle
 
 class Rover 
 {
 
-	
+	int x;
+	int y;
+	int heading;
 }
 
 public class MissionControl
 {
-
+	// A 2d array the size of the plateau. Can be used to hold position of 
+	// rovers, obstacles etc. So maneavouring is easier.
+	String plateau [][]; 
+	char headings [] = {'N', 'E', 'S', 'W'};
+	
 	public static void main(String[] args)
 	{
+		// Scan the first line separately as its format is different.
+		Scanner parser =  new Scanner (args[0]);
+		// Initialise the size of the plateau
+		plateau = new String[parser.nextInt()][parser.nextInt()];
+		parser.close();
+
 		for (String input : args)
 		{
 			Scanner parser = new Scanner(input);
+
+			parser.close(); 
 		}	
 	}
 }
