6d5b733c915a6a2568d69e566bf1295e63b7f7be
Neha Agrawal <neha.agrawal3093@gmail.com>
2014-03-20 18:35:56 +0000

    Added parsing for remaining input

--- MissionControl.java
+++ MissionControl.java
@@ -6,6 +6,15 @@ class Rover
 	int x;
 	int y;
 	int heading;
+	String movement;
+
+	public Rover(int xc, int yc, int h, String m)
+	{
+		x = xc;
+		y = yc;
+		heading = h;
+		movement = m;
+	}
 }
 
 public class MissionControl
@@ -13,8 +22,24 @@ public class MissionControl
 	// A 2d array the size of the plateau. Can be used to hold position of 
 	// rovers, obstacles etc. So maneavouring is easier.
 	String plateau [][]; 
+	// Need a better way to do this
 	char headings [] = {'N', 'E', 'S', 'W'};
-	
+	// Array of rovers
+	Rover roversOnMars;
+
+	// Here the assumption is that letters will always be entered in uppercase
+	private int findDirection(char compass)
+	{
+		if(compass == 'N')
+			return 0;
+		else if(compass == 'E')
+			return 1;
+		else if(compass == 'S')
+			return 2;
+		else 
+			return 3;
+	}
+
 	public static void main(String[] args)
 	{
 		// Scan the first line separately as its format is different.
@@ -23,11 +48,22 @@ public class MissionControl
 		plateau = new String[parser.nextInt()][parser.nextInt()];
 		parser.close();
 
-		for (String input : args)
-		{
-			Scanner parser = new Scanner(input);
+		// Initialise rover array
+		roversOnMars = new Rover[(args.length - 1)/2];
 
-			parser.close(); 
+		String input;
+		int x_coor, y_coor, heading;
+		String movement;
+		for (int index = 1; index<args.length; index=index+2)
+		{
+			input = args[index];
+			Scanner parser = new Scanner(input);
+			x_coor = parser.nextInt();
+			y_coor = parser.nextInt();
+			heading = findDirection(parser.nextChar());
+			parser.close();
+			// Get movement
+			movement = args[++index];
 		}	
 	}
 }
